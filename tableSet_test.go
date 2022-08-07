f25ea17c69ab3519ae04cc326068625ae405e035
steden <1470804@qq.com>
2022-08-07 19:28:15 +0800

    迁移子库

---
+++ tableSet_test.go
@@ -0,0 +1,16 @@
+package data
+
+import (
+	"github.com/farseer-go/fs/configure"
+	"log"
+	"testing"
+)
+
+func TestTableSet_ToList(t *testing.T) {
+	// 设置配置默认值，模拟配置文件
+	configure.SetDefault("Database.test", "DataType=MySql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
+	context := NewContext[TestMysqlContext]("test")
+
+	list := context.User.Select("Age").ToList()
+	log.Println(list)
+}
