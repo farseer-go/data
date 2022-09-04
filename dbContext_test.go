24b2043bd13b9e23dd016cbce42853170e0cccf3
steden <1470804@qq.com>
2022-09-05 00:41:36 +0800

    单元测试覆盖率：45.5%

--- dbContext_test.go
+++ dbContext_test.go
@@ -1,6 +1,11 @@
 package data
 
 import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/farseer-go/collections"
 	"github.com/farseer-go/fs/configure"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -16,14 +21,73 @@ type UserPO struct {
 	Name string
 	// 用户年龄
 	Age int
+	// 用户全称
+	Fullname FullNameVO
+	// 特长
+	Specialty collections.List[string]
+	// 自定义属性
+	Attribute collections.Dictionary[string, string]
+	// 性别
+	Gender GenderType
 }
+
+// 全称
+type FullNameVO struct {
+	// 姓氏
+	FirstName string
+	// 名称
+	LastName string
+}
+
+// Value return json value, implement driver.Valuer interface
+func (receiver FullNameVO) Value() (driver.Value, error) {
+	ba, err := json.Marshal(receiver)
+	return string(ba), err
+}
+
+// Scan scan value into Jsonb, implements sql.Scanner interface
+func (receiver *FullNameVO) Scan(val any) error {
+	if val == nil {
+		*receiver = FullNameVO{}
+		return nil
+	}
+	var ba []byte
+	switch v := val.(type) {
+	case []byte:
+		ba = v
+	case string:
+		ba = []byte(v)
+	default:
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", val))
+	}
+
+	t := FullNameVO{}
+	err := json.Unmarshal(ba, &t)
+	*receiver = t
+	return err
+}
+
+// MarshalJSON to output non base64 encoded []byte
+//func (receiver FullNameVO) MarshalJSON() ([]byte, error) {
+//	return json.Marshal(receiver)
+//}
+
+// UnmarshalJSON to deserialize []byte
+//func (receiver *FullNameVO) UnmarshalJSON(b []byte) error {
+//	return json.Unmarshal(b, &receiver)
+//}
+
+type GenderType int
+
+const (
+	Man GenderType = iota
+	Woman
+)
 
 func TestNewContext(t *testing.T) {
 	// 设置配置默认值，模拟配置文件
 	configure.SetDefault("Database.test", "DataType=MySql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
-
 	context := NewContext[TestMysqlContext]("test")
-
 	assert.Equal(t, "user", context.User.GetTableName())
 }
 
@@ -44,4 +108,6 @@ func TestInitContext(t *testing.T) {
 
 	InitContext(context2, "test")
 	assert.Equal(t, "user", context2.User.GetTableName())
+	context2.User.SetTableName("user2")
+	assert.Equal(t, "user2", context2.User.GetTableName())
 }
--- tableSet_test.go
+++ tableSet_test.go
@@ -1,16 +1,57 @@
 package data
 
 import (
+	"github.com/farseer-go/collections"
+	"github.com/farseer-go/fs"
 	"github.com/farseer-go/fs/configure"
 	"github.com/farseer-go/fs/flog"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestTableSet_ToList(t *testing.T) {
+	Module{}.Shutdown()
+	fs.Initialize[Module]("test data")
 	// 设置配置默认值，模拟配置文件
 	configure.SetDefault("Database.test", "DataType=MySql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
-	context := NewContext[TestMysqlContext]("test")
+	var context TestMysqlContext
+	InitContext(&context, "test")
 
-	list := context.User.Select("Age").ToList()
+	// 先清空数据
+	context.User.Where("1=1").Delete()
+	// 此时的数据量应该为0
+	assert.Equal(t, int64(0), context.User.Count())
+
+	context.User.Insert(&UserPO{
+		Name: "steden",
+		Age:  36,
+		Fullname: FullNameVO{
+			FirstName: "he",
+			LastName:  "steden",
+		},
+		Specialty: collections.NewList("go", "net"),
+		Attribute: collections.NewDictionaryFromMap(map[string]string{"work-year": "15"}),
+		Gender:    Man,
+	})
+
+	context.User.Insert(&UserPO{
+		Name: "harlen",
+		Age:  34,
+		Fullname: FullNameVO{
+			FirstName: "lee",
+			LastName:  "harlen",
+		},
+		Specialty: collections.NewList("go", "net"),
+		Attribute: collections.NewDictionaryFromMap(map[string]string{"work-year": "10"}),
+		Gender:    Woman,
+	})
+
+	// 此时的数据量应该为0
+	assert.Equal(t, int64(2), context.User.Count())
+	list := context.User.Select("Age").Select("Name").Where("Age > ?", 34).Where("Name = ?", "steden").ToList()
+	assert.Equal(t, 1, list.Count())
+
+	user := list.First()
+	assert.Equal(t, "steden", user.Name)
 	flog.Println(list)
 }
