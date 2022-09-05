145c11a19717a0c224cba3f8828b9b714a3e25a5
steden <1470804@qq.com>
2022-09-05 20:22:58 +0800

    单元测试覆盖率：55.6%

--- dbContext_test.go
+++ dbContext_test.go
@@ -76,23 +76,31 @@ const (
 )
 
 func TestNewContext(t *testing.T) {
-	assert.Panics(t, func() {
-		NewContext[TestMysqlContext]("")
+	t.Run("withoutDbName", func(t *testing.T) {
+		assert.Panics(t, func() {
+			NewContext[TestMysqlContext]("")
+		})
 	})
 
-	assert.Panics(t, func() {
-		NewContext[TestMysqlContext]("test")
+	t.Run("NotSetConfig", func(t *testing.T) {
+		assert.Panics(t, func() {
+			NewContext[TestMysqlContext]("test")
+		})
 	})
 
-	assert.Panics(t, func() {
-		configure.SetDefault("Database.test", "DataType=MySql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
-		NewContext[TestMysqlContext]("test")
+	t.Run("errorPwd", func(t *testing.T) {
+		assert.Panics(t, func() {
+			configure.SetDefault("Database.test", "DataType=MySql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
+			NewContext[TestMysqlContext]("test")
+		})
 	})
 
-	// 设置配置默认值，模拟配置文件
-	configure.SetDefault("Database.test", "DataType=MySql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
-	context := NewContext[TestMysqlContext]("test")
-	assert.Equal(t, "user", context.User.GetTableName())
+	t.Run("NewContext", func(t *testing.T) {
+		// 设置配置默认值，模拟配置文件
+		configure.SetDefault("Database.test", "DataType=MySql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
+		context := NewContext[TestMysqlContext]("test")
+		assert.Equal(t, "user", context.User.GetTableName())
+	})
 }
 
 func TestInitContext(t *testing.T) {
@@ -100,58 +108,69 @@ func TestInitContext(t *testing.T) {
 	configure.SetDefault("Database.test", "DataType=MySql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
 
 	var context TestMysqlContext
-	InitContext(&context, "test")
-	assert.Equal(t, "user", context.User.GetTableName())
-
-	InitContext(&context, "test")
-	assert.Equal(t, "user", context.User.GetTableName())
-
-	context2 := new(TestMysqlContext)
-	InitContext(context2, "test")
-	assert.Equal(t, "user", context2.User.GetTableName())
-
-	InitContext(context2, "test")
-	assert.Equal(t, "user", context2.User.GetTableName())
-	context2.User.SetTableName("user2")
-	assert.Equal(t, "user2", context2.User.GetTableName())
-}
-
-func Test_checkConfig_empty(t *testing.T) {
-	assert.Panics(t, func() {
-		configure.SetDefault("Database.test", "")
-		fs.Initialize[Module]("test data")
+	t.Run("zero value", func(t *testing.T) {
+		InitContext(&context, "test")
+		assert.Equal(t, "user", context.User.GetTableName())
 	})
-}
 
-func Test_checkConfig_emptyConnection(t *testing.T) {
-	assert.Panics(t, func() {
-		configure.SetDefault("Database.test", "DataType=MySql,PoolMaxSize=50,PoolMinSize=1")
-		fs.Initialize[Module]("test data")
+	t.Run("have value", func(t *testing.T) {
+		InitContext(&context, "test")
+		assert.Equal(t, "user", context.User.GetTableName())
 	})
-}
 
-func Test_checkConfig_emptyDataType(t *testing.T) {
-	assert.Panics(t, func() {
-		configure.SetDefault("Database.test", "PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
-		fs.Initialize[Module]("test data")
-	})
-}
+	t.Run("ptr", func(t *testing.T) {
+		context2 := new(TestMysqlContext)
+		InitContext(context2, "test")
+		assert.Equal(t, "user", context2.User.GetTableName())
 
-func Test_checkConfig_unknownDataType(t *testing.T) {
-	assert.Panics(t, func() {
-		configure.SetDefault("Database.test", "DataType=oracle,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
-		NewContext[TestMysqlContext]("test")
+		InitContext(context2, "test")
+		assert.Equal(t, "user", context2.User.GetTableName())
+		context2.User.SetTableName("user2")
+		assert.Equal(t, "user2", context2.User.GetTableName())
 	})
 }
 
 func Test_checkConfig(t *testing.T) {
-	//configure.SetDefault("Database.test", "DataType=postgresql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
-	//NewContext[TestMysqlContext]("test")
+	t.Run("empty", func(t *testing.T) {
+		assert.Panics(t, func() {
+			configure.SetDefault("Database.test", "")
+			fs.Initialize[Module]("test data")
+		})
+	})
 
-	//configure.SetDefault("Database.test", "DataType=sqlite,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
-	//NewContext[TestMysqlContext]("test")
+	t.Run("emptyConnection", func(t *testing.T) {
+		assert.Panics(t, func() {
+			configure.SetDefault("Database.test", "DataType=MySql,PoolMaxSize=50,PoolMinSize=1")
+			fs.Initialize[Module]("test data")
+		})
+	})
 
-	//configure.SetDefault("Database.test", "DataType=sqlserver,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
-	//NewContext[TestMysqlContext]("test")
+	t.Run("emptyDataType", func(t *testing.T) {
+		assert.Panics(t, func() {
+			configure.SetDefault("Database.test", "PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
+			fs.Initialize[Module]("test data")
+		})
+	})
 
+	t.Run("unknownDataType", func(t *testing.T) {
+		assert.Panics(t, func() {
+			configure.SetDefault("Database.test", "DataType=oracle,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
+			NewContext[TestMysqlContext]("test")
+		})
+	})
+
+	t.Run("postgresql", func(t *testing.T) {
+		//configure.SetDefault("Database.test", "DataType=postgresql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
+		//NewContext[TestMysqlContext]("test")
+	})
+
+	t.Run("sqlite", func(t *testing.T) {
+		//configure.SetDefault("Database.test", "DataType=sqlite,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
+		//NewContext[TestMysqlContext]("test")
+	})
+
+	t.Run("sqlserver", func(t *testing.T) {
+		//configure.SetDefault("Database.test", "DataType=sqlserver,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(mysql:3306)/test?charset=utf8&parseTime=True&loc=Local")
+		//NewContext[TestMysqlContext]("test")
+	})
 }
--- tableSet_test.go
+++ tableSet_test.go
@@ -16,57 +16,82 @@ func TestTableSet_ToList(t *testing.T) {
 	var context TestMysqlContext
 	InitContext(&context, "test")
 
-	// 先清空数据
-	context.User.Where("1=1").Delete()
-	// 此时的数据量应该为0
-	assert.Equal(t, int64(0), context.User.Count())
-
-	context.User.Insert(&UserPO{
-		Name: "steden",
-		Age:  36,
-		Fullname: FullNameVO{
-			FirstName: "he",
-			LastName:  "steden",
-		},
-		Specialty: collections.NewList("go", "net"),
-		Attribute: collections.NewDictionaryFromMap(map[string]string{"work-year": "15"}),
-		Gender:    Man,
+	t.Run("delete", func(t *testing.T) {
+		// 先清空数据
+		context.User.Where("1=1").Delete()
+		// 此时的数据量应该为0
+		assert.Equal(t, int64(0), context.User.Count())
 	})
 
-	context.User.Insert(&UserPO{
-		Name: "harlen",
-		Age:  34,
-		Fullname: FullNameVO{
-			FirstName: "lee",
-			LastName:  "harlen",
-		},
-		Specialty: collections.NewList("go", "net"),
-		Attribute: collections.NewDictionaryFromMap(map[string]string{"work-year": "10"}),
-		Gender:    Woman,
-	})
+	t.Run("insert", func(t *testing.T) {
+		context.User.Insert(&UserPO{
+			Name: "steden",
+			Age:  36,
+			Fullname: FullNameVO{
+				FirstName: "he",
+				LastName:  "steden",
+			},
+			Specialty: collections.NewList("go", "net"),
+			Attribute: collections.NewDictionaryFromMap(map[string]string{"work-year": "15"}),
+			Gender:    Man,
+		})
 
-	// 此时的数据量应该为2
-	assert.Equal(t, int64(2), context.User.Count())
-	assert.Equal(t, int64(2), context.User.SetTableName("user").Count())
+		context.User.Insert(&UserPO{
+			Name: "harlen",
+			Age:  34,
+			Fullname: FullNameVO{
+				FirstName: "lee",
+				LastName:  "harlen",
+			},
+			Specialty: collections.NewList("go", "net"),
+			Attribute: collections.NewDictionaryFromMap(map[string]string{"work-year": "10"}),
+			Gender:    Woman,
+		})
+
+		// 此时的数据量应该为2
+		assert.Equal(t, int64(2), context.User.Count())
+		assert.Equal(t, int64(2), context.User.SetTableName("user").Count())
+	})
 
 	// 测试条件筛选、字段筛选
-	list := context.User.Select("Age").Select("Name", "Id").Where("Age > ?", 34).Where("Name = ?", "steden").ToList()
-	assert.Equal(t, 1, list.Count())
-	assert.Equal(t, "steden", list.First().Name)
-	assert.Equal(t, 36, list.First().Age)
-	assert.Less(t, 1, list.First().Id)
+	t.Run("select", func(t *testing.T) {
+		lst := context.User.Select("Age").Select("Name", "Id").Where("Age > ?", 34).Where("Name = ?", "steden").ToList()
+		assert.Equal(t, 1, lst.Count())
+		assert.Equal(t, "steden", lst.First().Name)
+		assert.Equal(t, 36, lst.First().Age)
+		assert.Equal(t, 0, lst.First().Attribute.Count())
+		assert.Equal(t, "", lst.First().Fullname.FirstName)
+		assert.Equal(t, "", lst.First().Fullname.LastName)
+		assert.Equal(t, Man, lst.First().Gender)
+		assert.Equal(t, 0, lst.First().Specialty.Count())
+		assert.Less(t, 1, lst.First().Id)
+	})
 
 	// 测试排序
-	lst := context.User.Asc("Age").ToList()
-	assert.Equal(t, 2, lst.Count())
-	assert.Equal(t, "harlen", lst.First().Name)
+	t.Run("asc", func(t *testing.T) {
+		lst := context.User.Asc("Age").ToList()
+		assert.Equal(t, 2, lst.Count())
+		assert.Equal(t, "harlen", lst.First().Name)
+	})
 
-	lst = context.User.Desc("Age").ToList()
-	assert.Equal(t, 2, lst.Count())
-	assert.Equal(t, "steden", lst.First().Name)
+	t.Run("desc", func(t *testing.T) {
+		lst := context.User.Desc("Age").ToList()
+		assert.Equal(t, 2, lst.Count())
+		assert.Equal(t, "steden", lst.First().Name)
+	})
 
-	lst = context.User.Limit(1).ToList()
-	assert.Equal(t, 1, lst.Count())
+	t.Run("limit", func(t *testing.T) {
+		lst := context.User.Limit(1).ToList()
+		assert.Equal(t, 1, lst.Count())
+		assert.Equal(t, "steden", lst.First().Name)
+		assert.Equal(t, 36, lst.First().Age)
+		assert.Equal(t, 1, lst.First().Attribute.Count())
+		assert.Equal(t, "he", lst.First().Fullname.FirstName)
+		assert.Equal(t, "steden", lst.First().Fullname.LastName)
+		assert.Equal(t, Man, lst.First().Gender)
+		assert.Equal(t, 2, lst.First().Specialty.Count())
+		assert.Less(t, 1, lst.First().Id)
+	})
 }
 
 func TestTableSet_Limit(t *testing.T) {
