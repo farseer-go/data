23a9438ce706430158d9201dce63ffe2d9d756e2
steden <1470804@qq.com>
2023-02-28 13:00:00 +0800

    调整日志输出格式

--- healthCheck.go
+++ healthCheck.go
@@ -9,5 +9,5 @@ func (c *healthCheck) Check() (string, error) {
 	InitContext(c, c.name)
 	c.Table.open()
 	defer c.Table.close()
-	return "数据库 " + c.name, c.Table.err
+	return "Database." + c.name, c.Table.err
 }
