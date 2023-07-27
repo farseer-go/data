1b1427de3122083e94867ee0e79f782e55ede5d3
steden <1470804@qq.com>
2023-07-27 20:58:28 +0800

    调整单元测试用例

---
+++ test/init_test.go
@@ -0,0 +1,13 @@
+package test
+
+import (
+	"github.com/farseer-go/data"
+	"github.com/farseer-go/fs"
+	"github.com/farseer-go/fs/configure"
+)
+
+func init() {
+	// 设置配置默认值，模拟配置文件
+	configure.SetDefault("Database.test", "DataType=MySql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(192.168.1.8:3306)/test?charset=utf8&parseTime=True&loc=Local")
+	fs.Initialize[data.Module]("test data")
+}
