bc15870ce501e2cfcc92649d62978e9963e29089
steden <1470804@qq.com>
2023-10-27 11:33:03 +0800

    调整：Update方法转map时，json需要单独处理

--- utils.go
+++ utils.go
@@ -1,30 +1,48 @@
 package data
 
 import (
+	"encoding/json"
 	"go/ast"
 	"gorm.io/gorm/schema"
 	"reflect"
 )
 
+// ToMap PO实体转map
 func ToMap(entity any) map[string]any {
 	dic := make(map[string]any)
 	dicValue := reflect.ValueOf(dic)
 	fsVal := reflect.Indirect(reflect.ValueOf(entity))
 	namingStrategy := schema.NamingStrategy{IdentifierMaxLength: 64}
 
+	// 遍历字段
 	for i := 0; i < fsVal.NumField(); i++ {
 		var fieldName string
+		// 取出当前字段
 		if field := fsVal.Type().Field(i); ast.IsExported(field.Name) {
 			fieldName = field.Name
+			// 取出gorm标签
 			fieldTags := schema.ParseTagSetting(field.Tag.Get("gorm"), ";")
-			if c, existsColumn := fieldTags["COLUMN"]; existsColumn {
-				fieldName = c
-				continue
+			// 取出Column标签
+			if colName, existsColumn := fieldTags["COLUMN"]; existsColumn {
+				fieldName = colName
+			} else {
+				fieldName = namingStrategy.ColumnName("", field.Name)
 			}
-			fieldName = namingStrategy.ColumnName("", field.Name)
+
+			// 取出json标签
+			_, isJsonField := fieldTags["JSON"]
+			if !isJsonField {
+				jsonTag, existsJsonTag := fieldTags["SERIALIZER"]
+				isJsonField = existsJsonTag && jsonTag == "json"
+			}
+
+			itemValue := fsVal.Field(i)
+			if isJsonField {
+				marshal, _ := json.Marshal(itemValue.Interface())
+				itemValue = reflect.ValueOf(marshal)
+			}
+			dicValue.SetMapIndex(reflect.ValueOf(fieldName), itemValue)
 		}
-		itemValue := fsVal.Field(i)
-		dicValue.SetMapIndex(reflect.ValueOf(fieldName), itemValue)
 	}
 	return dic
 }
