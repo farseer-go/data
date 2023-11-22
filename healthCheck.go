f18f7076f91c5e49c5d57134bfcb94f09a85fd17
steden <1470804@qq.com>
2023-11-22 22:52:23 +0800

    移除go 1.20方法

--- healthCheck.go
+++ healthCheck.go
@@ -14,5 +14,5 @@ func (c *healthCheck) Check() (string, error) {
 	InitContext(c, c.name)
 	var dbAt time.Time
 	tx := c.Original().Raw("select now()").Scan(&dbAt)
-	return fmt.Sprintf("Database.%s => %s", c.name, dbAt.Format(time.DateTime)), tx.Error
+	return fmt.Sprintf("Database.%s => %s", c.name, dbAt.Format("2006-01-02 15:04:05")), tx.Error
 }
